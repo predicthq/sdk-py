6cbf09fa92abd80248af09815a0aa85cc8551287
Thierry Jossermoz <thierry.jossermoz@oohlalabs.com>
2016-03-31 15:06:48 +1300

    Fix industry

--- predicthq/endpoints/v1/accounts/schemas.py
+++ predicthq/endpoints/v1/accounts/schemas.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import, print_function
 
-from predicthq.endpoints.schemas import Model, StringType, DateTimeType
+from predicthq.endpoints.schemas import Model, StringType, DateTimeType, ModelType
+
+
+class Industry(Model):
+    
+    id = StringType()
+    name = StringType()
 
 
 class Account(Model):
@@ -9,6 +15,6 @@ class Account(Model):
     id = StringType()
     name = StringType()
     description = StringType()
-    industry = StringType()
+    industry = ModelType(Industry)
     created_at = DateTimeType()
     updated_at = DateTimeType()
