928060b771db38ed0b78fdcddd0e31bb0ec72ad5
Chloe Tao <chloe@predicthq.com>
2020-02-10 14:27:46 +1300

    update comment

--- usecases/events/retrieve_aggregate_event_impact.py
+++ usecases/events/retrieve_aggregate_event_impact.py
@@ -12,13 +12,13 @@ phq = Client(access_token=ACCESS_TOKEN)
 # We always aggregate on the date but you can use different impact_rank values to specify how we calculate the impact.
 # You will need Data License to access this endpoint and Aviation Rank subscription for Aviation Rank impact.
 
-# You can either aggregate on PHQ Rank
+# You can either aggregate on PHQ Rank by passing impact_rank param as 'rank'.
 # https://developer.predicthq.com/resources/events/#param-impact-rank
 for impact_day in phq.events.impact(active__gte='2015-12-24', active__lte='2015-12-26', impact_rank='rank'):
     print(impact_day.date, impact_day.impact, impact_day.rank_levels, impact_day.rank_levels_impact, impact_day.categories, impact_day.categories_impact)
 
 
-# or aggregate on Aviation Rank
+# or aggregate on Aviation Rank by passing impact_rank param as 'aviation_rank'.
 # https://developer.predicthq.com/resources/events/#param-impact-rank
 for impact_day in phq.events.impact(active__gte='2015-12-24', active__lte='2015-12-26', impact_rank='aviation_rank'):
     print(impact_day.date, impact_day.impact, impact_day.aviation_rank_levels, impact_day.aviation_rank_levels_impact, impact_day.categories, impact_day.categories_impact)
