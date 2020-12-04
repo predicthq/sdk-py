a6b438ca821394b67035387d102fa83985a6a407
Matthieu Bonnefoy <matthieu@predicthq.com>
2020-12-04 14:00:05 +1300

    Move aei examples to use case dir

--- usecases/aei_from_events/aei_from_events.py
+++ usecases/aei_from_events/aei_from_events.py
@@ -1,19 +1,21 @@
+from datetime import datetime, timedelta
+
+import pandas as pd
+
 from predicthq import Client
 
 # Please copy paste your access token here
 # or read our Quickstart documentation if you don't have a token yet
 # https://docs.predicthq.com/guides/quickstart/
-
 EVENT_ENDPOINT_COLUMNS = ['id', 'title', 'description', 'start', 'end',
-       'duration', 'labels', 'category', 'timezone', 'country', 'location',
-       'scope', 'rank', 'local_rank',
-       'phq_attendance', 'state', 'deleted_reason', 'first_seen']
+                          'duration', 'labels', 'category', 'timezone', 'country', 'location',
+                          'scope', 'rank', 'local_rank', 'phq_attendance', 'state',
+                          'deleted_reason', 'first_seen']
 
-ACCESS_TOKEN = '' # USER TO INCLUDE THEIR OWN TOKEN
+
+ACCESS_TOKEN = ''  # USER TO INCLUDE THEIR OWN TOKEN
 phq = Client(access_token=ACCESS_TOKEN)
 
-import pandas as pd
-from datetime import datetime, timedelta
 
 # Definition: Events with a start and end date and time.
 #             PredictHQ models predict attendances for each of these events.
@@ -33,6 +35,7 @@ UNSCHEDULED_CATEGORIES = ['airport-delays', 'disasters', 'health-warnings',
 # represents the collective sum of all event categories
 ALL_CATEGORIES = ATTENDANCE_CATEGORIES + NON_ATTENDANCE_CATEGORIES + UNSCHEDULED_CATEGORIES
 
+
 def generate_calendar(start, end):
     """
     Generate a list of dates between a start and end period.
@@ -50,6 +53,7 @@ def generate_calendar(start, end):
         date_range.append(str(temp_date).split(' ')[0])
     return date_range
 
+
 def pivot_event_impact_by_event_type(cal, events_pd):
     """Returns a dataframe with the summed impact (daily phq_attendance) across
        all categories. This dataframe imputes zeros for days with no events
@@ -66,17 +70,18 @@ def pivot_event_impact_by_event_type(cal, events_pd):
         qualified_events_agg = (qualified_events.groupby('category')['impact']
                                                 .sum().reset_index())
         qualified_events_pivot = (pd.pivot_table(qualified_events_agg,
-                                                 columns = 'category',
-                                                 values = 'impact',
+                                                 columns='category',
+                                                 values='impact',
                                                  fill_value=0))
 
         # append the current day to the pivoted result
-        d_col = pd.DataFrame([day], columns = ['date'])
-        aei_temp = pd.concat([d_col, qualified_events_pivot.reset_index(drop=True)], axis = 1)
+        d_col = pd.DataFrame([day], columns=['date'])
+        aei_temp = pd.concat([d_col, qualified_events_pivot.reset_index(drop=True)], axis=1)
         all_pd.append(aei_temp)
     return all_pd
 
-def create_aggregate_event_impact(df, event_groupings = ATTENDANCE_CATEGORIES):
+
+def create_aggregate_event_impact(df, event_groupings=ATTENDANCE_CATEGORIES):
     """
     Provided a dataframe of events (typically pulled from the events endpoint),
     transform into a format that is easy to use for improving forecasting.
@@ -86,15 +91,16 @@ def create_aggregate_event_impact(df, event_groupings = ATTENDANCE_CATEGORIES):
     category type, so it is recommended that each of the three event
     category groupings are treated independently.
     """
-    att_df = df[df.category.isin(event_groupings) == True]
+    att_df = df[df.category.isin(event_groupings) is True]
     att_df['days_duration'] = [1 if x / 86400 < 1 else round(x / 86400, 0) for x in att_df.duration]
     att_df['impact'] = round(att_df.phq_attendance / att_df.days_duration, 0)
     start_cal, end_cal = att_df.start.min(), att_df.end.max()
     print(start_cal, end_cal)
     df_cal = generate_calendar(start_cal, end_cal)
 
-    aei_pd = pd.concat(pivot_event_impact_by_event_type(df_cal, att_df), axis = 0).fillna(0)
+    aei_pd = pd.concat(pivot_event_impact_by_event_type(df_cal, att_df), axis=0).fillna(0)
     return aei_pd
+
 
 # Example Usage
 # Step 1 - Query for events
@@ -114,8 +120,8 @@ seattle_query = (phq.events.search(start=start,
 
 # Transform query result into a dataframe
 expanded_events = [[event[column] for column in EVENT_ENDPOINT_COLUMNS] for event in [*seattle_query]]
-all_seattle_events = pd.DataFrame(expanded_events, columns = EVENT_ENDPOINT_COLUMNS)
+all_seattle_events = pd.DataFrame(expanded_events, columns=EVENT_ENDPOINT_COLUMNS)
 
 # Step 2 - Transform the events into an Aggregate Event Impact (AEI) Dataframe
 seattle_aei = (create_aggregate_event_impact(all_seattle_events,
-                                             event_groupings = ATTENDANCE_CATEGORIES))
+                                             event_groupings=ATTENDANCE_CATEGORIES))
