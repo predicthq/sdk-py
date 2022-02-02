09714b94a41238bc0a64fd437014dea01e6ae810
Matthieu Bonnefoy <matthieu@predicthq.com>
2022-02-03 10:25:08 +1300

    Add config mixin to oauth endpoints

--- predicthq/endpoints/oauth2/endpoint.py
+++ predicthq/endpoints/oauth2/endpoint.py
@@ -6,18 +6,28 @@ from .schemas import AccessToken, GetTokenParams, RevokeTokenParams
 class OAuth2Endpoint(BaseEndpoint):
     @accepts(GetTokenParams)
     @returns(AccessToken)
-    def get_token(self, client_id, client_secret, scope, grant_type, **kwargs):
+    def get_token(self, verify_ssl, client_id, client_secret, scope, grant_type, **kwargs):
         data = {
             "grant_type": grant_type,
             "scope": scope,
         }
         data.update(kwargs)
-        return self.client.post("/oauth2/token/", auth=(client_id, client_secret), data=data)
+        return self.client.post(
+            "/oauth2/token/",
+            auth=(client_id, client_secret),
+            data=data,
+            verify=verify_ssl,
+        )
 
     @accepts(RevokeTokenParams)
-    def revoke_token(self, client_id, client_secret, token, token_type_hint):
+    def revoke_token(self, verify_ssl, client_id, client_secret, token, token_type_hint):
         data = {
             "token_type_hint": token_type_hint,
             "token": token,
         }
-        self.client.post("/oauth2/revoke/", auth=(client_id, client_secret), data=data)
+        self.client.post(
+            "/oauth2/revoke/",
+            auth=(client_id, client_secret),
+            data=data,
+            verify=verify_ssl,
+        )
