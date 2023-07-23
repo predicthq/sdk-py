c7e62174120e20cadbe136fc168bc0720e442912
Keegan Cordeiro <keegan.cordeiro@predicthq.com>
2023-07-24 10:48:17 +1200

    update usecases to include sorting

--- usecases/events/search_by_predicted_event_spend.py
+++ usecases/events/search_by_predicted_event_spend.py
@@ -15,7 +15,7 @@ phq = Client(access_token=ACCESS_TOKEN)
 
 # You can filter by predicted_event_spend when searching.
 # See https://docs.predicthq.com/resources/events/#param-predicted_event_spend
-for event in phq.events.search(predicted_event_spend={'gte': 5000, 'lte': 10000}):
+for event in phq.events.search(predicted_event_spend={'gte': 5000, 'lte': 10000}, sort="predicted_event_spend"):
     print(event.predicted_event_spend, event.category, event.title, event.start.strftime('%Y-%m-%d'))
     # The predicted_event_spend field will only contain a value if there is a predicted event
     # spend for the event and if your subscription includes the feature in your API plan.
@@ -23,7 +23,7 @@ for event in phq.events.search(predicted_event_spend={'gte': 5000, 'lte': 10000}
 
 # You can filter by predicted_event_spend_industry__accommodation when searching.
 # See https://docs.predicthq.com/resources/events/#param-predicted_event_spend
-for event in phq.events.search(predicted_event_spend_industry__accommodation={'gte': 5000, 'lte': 10000}):
+for event in phq.events.search(predicted_event_spend_industry__accommodation={'gte': 5000, 'lte': 10000}, sort="-predicted_event_spend_industry.accommodation"):
     print(event.predicted_event_spend_industries.accommodation, event.category, event.title, event.start.strftime('%Y-%m-%d'))
     # The predicted_event_spend field will only contain a value if there is a predicted event
     # spend for the event and if your subscription includes the feature in your API plan.
@@ -31,7 +31,7 @@ for event in phq.events.search(predicted_event_spend_industry__accommodation={'g
 
 # You can filter by predicted_event_spend_industry_hospitality when searching.
 # See https://docs.predicthq.com/resources/events/#param-predicted_event_spend
-for event in phq.events.search(predicted_event_spend_industry__hospitality={'gte': 5000, 'lte': 10000}):
+for event in phq.events.search(predicted_event_spend_industry__hospitality={'gte': 5000, 'lte': 10000}, sort="predicted_event_spend_industry.hospitality"):
     print(event.predicted_event_spend_industries.hospitality, event.category, event.title, event.start.strftime('%Y-%m-%d'))
     # The predicted_event_spend field will only contain a value if there is a predicted event
     # spend for the event and if your subscription includes the feature in your API plan.
@@ -39,7 +39,7 @@ for event in phq.events.search(predicted_event_spend_industry__hospitality={'gte
 
 # You can filter by predicted_event_spend_industry_transportation when searching.
 # See https://docs.predicthq.com/resources/events/#param-predicted_event_spend
-for event in phq.events.search(predicted_event_spend_industry__transportation={'gte': 5000, 'lte': 10000}):
+for event in phq.events.search(predicted_event_spend_industry__transportation={'gte': 5000, 'lte': 10000}, sort="predicted_event_spend_industry.transportation"):
     print(event.predicted_event_spend_industries.transportation, event.category, event.title, event.start.strftime('%Y-%m-%d'))
     # The predicted_event_spend field will only contain a value if there is a predicted event
     # spend for the event and if your subscription includes the feature in your API plan.
