c4e26a8ad8c09ae4de414d21f40cd06a324d70d4
Keegan <48084902+corke2013@users.noreply.github.com>
2024-03-28 12:52:39 +1300

    fix accept decorator bug (#90)
    * fix accept decorator bug
    
    * add tests for previous exception cases
    
    * remove duplicate code
    
    ---------
    
    Co-authored-by: Keegan Cordeiro <keegan.cordeiro@predicthq.com>

--- predicthq/endpoints/v1/features/endpoint.py
+++ predicthq/endpoints/v1/features/endpoint.py
@@ -7,7 +7,7 @@ from .schemas import FeatureResultSet
 
 class FeaturesEndpoint(UserBaseEndpoint):
 
-    BASE_FEATURE_CRITERIA = {"stats": ["sum", "count"], "phq_rank": None}
+    BASE_FEATURE_CRITERIA = {"stats": [["sum", "count"]], "phq_rank": [None]}
     FIELDS_TO_MUTATE = frozenset([
         "phq_attendance_",
         "phq_viewership_sports",
@@ -18,8 +18,8 @@ class FeaturesEndpoint(UserBaseEndpoint):
     @classmethod
     def mutate_bool_to_default_for_type(cls, user_request_spec):
         for key, val in user_request_spec.items():
-            if any(key.startswith(x) for x in cls.FIELDS_TO_MUTATE) and isinstance(val, bool):
-                user_request_spec[key] = cls.BASE_FEATURE_CRITERIA
+            if any(key.startswith(x) for x in cls.FIELDS_TO_MUTATE):
+                user_request_spec[key] = [cls.BASE_FEATURE_CRITERIA if isinstance(v, bool) else v for v in val]
 
     @accepts(query_string=False)
     @returns(FeatureResultSet)
