5f57567e1ac41b1a36ba5a0424e550e64d829b24
Medhat Gayed <51570025+medhatphq@users.noreply.github.com>
2024-07-17 13:55:17 +1200

    SE-1356 Modify features endpoint to dynamically set requested features. (#92)
    * SE-1356 Modify features endpoint to dynamically set requested features.
    
    * SE-1356 Fix TypeError: 'type' object is not subscriptable error for Python < 3.9
    
    * SE-1356 Simplify and improve type hints.
    
    * SE-1356 Black formatting.
    
    * SE-1356 Add notes on how to serialize search results and usage of .model_dump(exclude_none=True)
    
    * SE-1356 README update

--- predicthq/endpoints/v1/features/endpoint.py
+++ predicthq/endpoints/v1/features/endpoint.py
@@ -1,25 +1,27 @@
-import inspect
-
 from predicthq.endpoints.base import UserBaseEndpoint
 from predicthq.endpoints.decorators import accepts, returns
+
 from .schemas import FeatureResultSet
 
 
 class FeaturesEndpoint(UserBaseEndpoint):
-
     BASE_FEATURE_CRITERIA = {"stats": [["sum", "count"]], "phq_rank": [None]}
-    FIELDS_TO_MUTATE = frozenset([
-        "phq_attendance_",
-        "phq_viewership_sports",
-        "phq_impact_severe_weather_",
-        "phq_spend_"
-    ])
+    FIELDS_TO_MUTATE = frozenset(
+        [
+            "phq_attendance_",
+            "phq_viewership_",
+            "phq_impact_",
+            "phq_spend_",
+        ]
+    )
 
     @classmethod
     def mutate_bool_to_default_for_type(cls, user_request_spec):
         for key, val in user_request_spec.items():
             if any(key.startswith(x) for x in cls.FIELDS_TO_MUTATE):
-                user_request_spec[key] = [cls.BASE_FEATURE_CRITERIA if isinstance(v, bool) else v for v in val]
+                user_request_spec[key] = [
+                    cls.BASE_FEATURE_CRITERIA if isinstance(v, bool) else v for v in val
+                ]
 
     @accepts(query_string=False)
     @returns(FeatureResultSet)
