9ff48c271933dc300e92e96f4c4d4a7e27934e9a
Keegan Cordeiro <keegan.cordeiro@predicthq.com>
2025-01-14 16:07:11 +1300

    add analysis group import

--- predicthq/endpoints/v1/beam/__init__.py
+++ predicthq/endpoints/v1/beam/__init__.py
@@ -1,5 +1,5 @@
 from .endpoint import BeamEndpoint
-from .schemas import Analysis
+from .schemas import Analysis, AnalysisGroup
 
 
-__all__ = ["BeamEndpoint", "Analysis"]
+__all__ = ["BeamEndpoint", "Analysis", "AnalysisGroup"]
